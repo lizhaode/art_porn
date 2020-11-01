82529515ebab24883dcf17c615bcb8e578e8b0b6
lizhaode <308697716@qq.com>
2020-11-01 22:59:24 +0800

    finish

---
+++ art_porn/pipelines.py
@@ -0,0 +1,32 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import requests
+
+from art_porn.items import ArtPornItem
+from art_porn.lib.download_header import random_other_headers
+from art_porn.spiders.Start import BaseSpider
+
+
+class ArtPornPipeline:
+    def process_item(self, item, spider: BaseSpider):
+        base_url = 'http://127.0.0.1:8900/jsonrpc'
+        token = 'token:' + spider.settings.get('ARIA_TOKEN')
+        if isinstance(item, ArtPornItem):
+            download_data = {
+                'jsonrpc': '2.0',
+                'method': 'aria2.addUri',
+                'id': '0',
+                'params': [token, [item['download_url']],
+                           {
+                               'out': item['title'] + '.mp4',
+                               'header': random_other_headers(),
+                               'dir': '/opt/videos/{0}'.format(item['category'])
+                           }]
+            }
+            requests.post(url=base_url, json=download_data)
+        return item
---
+++ art_porn/spiders/Start.py
@@ -0,0 +1,42 @@
+import scrapy
+from scrapy.http.response.html import HtmlResponse
+
+from art_porn.items import ArtPornItem
+
+
+class BaseSpider(scrapy.Spider):
+    name = 'all'
+
+    def start_requests(self):
+        body = {
+            'action': 'login',
+            'redirect_to': '/',
+            'format': 'json',
+            'mode': 'async',
+            'username': self.settings.get('USER'),
+            'pass': self.settings.get('PASS')
+        }
+        yield scrapy.FormRequest(url='https://theartporn.com/login/', formdata=body)
+
+    def parse(self, response: HtmlResponse, **kwargs):
+        if response.json().get('status') == 'success':
+            for category in self.settings.getlist('CATEGORY'):
+                yield scrapy.Request(url='https://theartporn.com/categories/{0}'.format(category),
+                                     callback=self.categories_parse, cb_kwargs={'category': category})
+
+    def categories_parse(self, response: HtmlResponse, category):
+        next_url_list = response.css('a.button.prev::attr(href)').getall()
+        if len(next_url_list) > 1:
+            yield scrapy.Request(url=response.urljoin(next_url_list[1]), callback=self.categories_parse)
+        else:
+            yield scrapy.Request(url=response.urljoin(next_url_list[0]), callback=self.categories_parse)
+
+        for item in response.css('div.thumb-video.cf').css('a.thumb-video-link::attr(href)').getall():
+            yield scrapy.Request(url=item, callback=self.video_parse, cb_kwargs={'category': category})
+
+    def video_parse(self, response: HtmlResponse, category):
+        title = response.css('h2.title.big::text').get()
+        for item in response.css('ul.video-downloads-buttons').css('li'):
+            if '1080p' in item.css('a::text').get().strip():
+                link = item.css('a::attr(href)').get()
+                yield ArtPornItem(name=title, link=link, category=category)
